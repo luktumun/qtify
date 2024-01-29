6e75111a4d26915867609ae1dbff83f36cf87b51
rajendra <30436194+luktumun@users.noreply.github.com>
2024-01-29 20:30:02 +0530

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 // import { Logo, Modals, Search1, SearchBar } from '../../components';
 import React from 'react';
-import { Logo, Modals, Search1 } from '../../components';
+import { Logo, Modals, SearchBar } from '../../components';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
@@ -11,13 +11,13 @@ const NavBar = ({ data }) => {
         <Link to="/">
           <Logo />
         </Link>
-        {/* <SearchBar data={data} className="searchbar" /> */}
-        <Search1 data={data} className="searchbar" />
+         <SearchBar data={data} className="searchbar" /> 
+       
 
         <Modals title={'Give Feedback'} />
       </nav>
-      {/* <SearchBar className="searchbar-moblie" /> */}
-      <Search1 data={data} className="searchbar-moblie" />
+     
+      <SearchBar data={data} className="searchbar-moblie" />
     </>
   );
 };

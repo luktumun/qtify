b0eaa5a645e6f890f7144ed7cbb7d44029c731d1
rajendrakhatana <srajendra923@gmail.com>
2024-01-29 18:43:26 +0530

    commites

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
+import headphones from "../../assets/headphones.png";
 import "./NavBar.css";
 import SearchBar from "../SearchBar/SearchBar";
 const NavBar = () => {
